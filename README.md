c1fb78c7ab882e89d00143f599e58a416d1247ec
Amir Sharapov <amir@amirfounder.com>
2022-05-20 21:07:28 -0500

    insert sorted

---
+++ README.md
@@ -0,0 +1,41 @@
+Topics to Cover:
+
+Core
+- algorithms
+  - complexity analysis
+  - graph traversals (BFS & DFS)
+  - A* algorithm
+  - Dijkstra Algorithm
+- data structures
+  - hash tables
+  - binary search tree
+  - suffix trees
+  - heaps
+- techniques
+  - sliding window
+  - recursion
+  - reverse a linked list
+  - invert binary tree
+  - greedy algorithms
+  - dynamic programming (DP)
+
+Practical
+- software engineering
+  - traveling salesman problem
+  - knapsack problem
+  - np complete
+- math
+  - probability and combinatorics
+  - n choose k problems
+- concurrency
+  - semaphores
+  - mutexes
+  - thread locks / deadlocks
+- os
+  - resource allocation
+  - context switching
+  - scheduling
+
+Interview problems
+- [geeks for geeks](https://www.geeksforgeeks.org/top-10-algorithms-in-interview-questions/)
+- [leetcode](https://practice.geeksforgeeks.org/explore?page=1&company[]=Google&sortBy=submissions)
---
+++ src/geeksforgeeks/linked_list.py
@@ -0,0 +1,48 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+def insert_sorted(root, value):
+    if not isinstance(value, (int, float)):
+        raise "Cannot store non-ints or non-floats"
+    return _insert_sorted(root, None, value)
+
+
+def _insert_sorted(current, prev, value):
+    if value == current.value:
+        node = Node(value)
+        node.next = current.next
+        current.next = node
+        return True
+    if value > current.value:
+        if current.next:
+            _insert_sorted(current.next, current, value)
+        else:
+            node = Node(value)
+            current.next = node
+            return True
+    if value < current.value:
+        node = Node(value)
+        if prev:
+            prev.next = node
+        node.next = current
+        return True
+
+
+if __name__ == '__main__':
+    nodes = [Node(i) for i in range(10)]
+    for i, n in enumerate(nodes):
+        n.next = nodes[i + 1] if i < len(nodes) - 1 else None
+
+    n = nodes[0]
+    insert_sorted(n, -7)
+    insert_sorted(n, 7)
+    insert_sorted(n, 100)
+    insert_sorted(n, 8.5)
+    insert_sorted(n, -4)
+
+    while n:
+        print(n.value)
+        n = n.next
