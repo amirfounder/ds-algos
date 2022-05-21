cfcba68b6964dbce218effed7fb695c76ed568b5
Amir Sharapov <amir@amirfounder.com>
2022-05-20 22:04:27 -0500

    comparison

--- src/geeksforgeeks/linked_list.py
+++ src/geeksforgeeks/linked_list.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+
+
 class Node:
     def __init__(self, value):
         self.value = value
@@ -24,16 +27,57 @@ class Node:
 
 
 class LinkedList:
-    def __init__(self):
+    def __init__(self, values=None):
         self.head = None
+        if values:
+            for value in values:
+                self.insert(value)
 
     def __str__(self):
-        s = ['Nodes']
+        s = []
         c = self.head
         while c:
             s.append(f'{c.value}')
             c = c.next
-        return ' > '.join(s)
+        return 'Linked List Nodes: ' + ' > '.join(s)
+
+    def compare(self, other: LinkedList):
+        p1 = self.head
+        p2 = other.head
+
+        while p1 and p2 and p1.value == p2.value:
+            p1 = p1.next
+            p2 = p2.next
+
+        if not p1 and not p2:
+            return 0
+
+        if not p2 or p1.value > p2.value:
+            return 1
+
+        if not p1 or p2.value > p1.value:
+            return -1
+
+        if p1.value == p2.value:
+            return 0
+
+    def delete(self, value):
+        prev = None
+        current = self.head
+
+        while current:
+            if value == current.value:
+                if prev:
+                    prev.next = current.next
+                else:
+                    self.head = self.head.next
+                return True
+
+            if not current.next:
+                return False
+
+            prev = current
+            current = current.next
 
     def insert_sorted(self, value):
         if not self.head:
@@ -47,8 +91,20 @@ class LinkedList:
         else:
             return self.head.insert_sorted(value)
 
+    def insert(self, value):
+        if not self.head:
+            self.head = Node(value)
+            return self.head
 
-if __name__ == '__main__':
+        current = self.head
+
+        while current.next:
+            current = current.next
+
+        current.next = Node(value)
+
+
+def main():
     llist = LinkedList()
     for i in range(10):
         llist.insert_sorted(i)
@@ -58,5 +114,31 @@ if __name__ == '__main__':
     llist.insert_sorted(100)
     llist.insert_sorted(8.5)
     llist.insert_sorted(-4)
-
+    llist.delete(7)
+    llist.delete(-7)
+    llist.delete(100)
     print(llist)
+
+    ll1 = LinkedList('hello')
+    ll2 = LinkedList('hello')
+    print(ll1.compare(ll2))
+
+    ll1 = LinkedList('')
+    ll2 = LinkedList('')
+    print(ll1.compare(ll2))
+
+    ll1 = LinkedList('1')
+    ll2 = LinkedList('')
+    print(ll1.compare(ll2))
+
+    ll1 = LinkedList('1')
+    ll2 = LinkedList('2')
+    print(ll1.compare(ll2))
+
+    ll1 = LinkedList('112')
+    ll2 = LinkedList('2')
+    print(ll1.compare(ll2))
+
+
+if __name__ == '__main__':
+    main()
