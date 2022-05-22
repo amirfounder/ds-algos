72a88823481185bd754ce3a8ed6483a5b2008943
Amir Sharapov <amir@amirfounder.com>
2022-05-22 11:58:41 -0500

    implemented buckets

--- src/core/data_structures/hashtable/chaining_hashtable.py
+++ src/core/data_structures/hashtable/chaining_hashtable.py
@@ -17,6 +17,29 @@ class ChainingHashNode:
         self.next = None
 
 
+class Bucket:
+    def __init__(self):
+        self.pairs = []
+
+    def is_empty(self):
+        return not self.pairs
+
+    def add(self, key, value):
+        for i, (k, v) in enumerate(self.pairs):
+            if k == key:
+                self.pairs[i] = (key, value)
+                return
+
+        self.pairs.append((key, value))
+
+    def get(self, key):
+        for k, v in self.pairs:
+            if k == key:
+                return v
+
+        return None
+
+
 class ChainingHashTable:
     def __init__(self):
         self.size: int = SIZE
@@ -59,9 +82,40 @@ class ChainingHashTable:
             self.data[hashed].append(node)
 
 
-if __name__ == '__main__':
+class ChainingHashTableWithBuckets:
+    def __init__(self):
+        self.size = 0
+        self.data = [Bucket() for _ in range(SIZE)]
+
+    def __str__(self):
+        values = []
+        for bucket in self.data:
+            for k, v in bucket.pairs:
+                values.append(f'{k}={v}')
+
+        return 'Chaining hashtable data: [' + ', '.join(values) + ']'
+
+    def get(self, key):
+        hashed = hash_fn(key)
+        return self.data[hashed].get(key)
+
+    def put(self, key, value):
+        hashed = hash_fn(key)
+        self.data[hashed].add(key, value)
+
+
+def main():
+    table = ChainingHashTableWithBuckets()
+    table.put('hello', 'goodbye')
+    table.put('key', 'value')
+    table.put('lol', 'haha')
+    print(table)
+
     table = ChainingHashTable()
     table.put('hello', 'goodbye')
-    v = table.get('hello')
-    print(v)
-    print(table)
+    table.put('key', 'value')
+    table.put('lol', 'haha')
+
+
+if __name__ == '__main__':
+    main()
